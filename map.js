830af75e7cc9d6f1714ee26d4bf9a03e122cabf3
ReshmiCode <rreshmiranjith@gmail.com>
2022-04-03 08:21:38 -0500

    switch to await/async

--- map.js
+++ map.js
@@ -6,11 +6,53 @@ var longitude = -96.750003;
 var startDate = "2021-10-01";
 var endDate = "2021-10-15";
 var minMagnitude = 5;
+var directionsService;
+var directionsDisplay;
+
+function addtoPoints(toAdd) { points.push(toAdd); }
 
 async function initialize() {
   geocoder = new google.maps.Geocoder();
   showMap(37, -96, 4);
+  directionsService = new google.maps.DirectionsService();
+  directionsDisplay = new google.maps.DirectionsRenderer();
 }
+
+const getAddress = address => {
+  return new Promise((resolve, reject) => {
+      geocoder.geocode({address: address}, (results, status) => {
+          if (status === 'OK') {
+              resolve(results[0]);
+          } else {
+              reject(status);
+          }    
+      });    
+  });
+};
+
+const getRoute = (request) => {
+  return new Promise((resolve, reject) => {
+      directionsService.route(request, (results, status) => {
+          if (status === 'OK') {
+              resolve(results);
+          } else {
+              reject(status);
+          }    
+      });    
+  });
+};
+
+const getStops = (service, stopRequest) => {
+  return new Promise((resolve, reject) => {
+      service.nearbySearch(stopRequest, (results, status) => {
+          if (status === 'OK') {
+              resolve(results[0]);
+          } else {
+              reject(status);
+          }    
+      });    
+  });
+};
 
 async function showRoute() {
   var start = document.getElementById("start").value;
@@ -18,129 +60,84 @@ async function showRoute() {
   var points = [];
 
   //Get coordinates of route endpoints
-  geocoder.geocode({ address: start }, function (results, status) {
-    if (status == "OK") {
-      latitude = results[0].geometry.location.lat();
-      longitude = results[0].geometry.location.lng();
-      var place = results[0].address_components[0].long_name;
-      points.push({ lat: latitude, lng: longitude });
-    } else {
-      alert("Geocode was not successful for the following reason: " + status);
-    }
-  });
-  geocoder.geocode({ address: dest }, function (results, status) {
-    if (status == "OK") {
-      latitude = results[0].geometry.location.lat();
-      longitude = results[0].geometry.location.lng();
-      var place = results[0].address_components[0].long_name;
-      points.push({ lat: latitude, lng: longitude });
-    } else {
-      alert("Geocode was not successful for the following reason: " + status);
-    }
-  });
-
-  console.log(points);
+  let startPoint = await getAddress(start);
+  points.push({lat: startPoint.geometry.location.lat(), long: startPoint.geometry.location.lng()});
 
   //Get directions
-  var directionsService = new google.maps.DirectionsService();
-  var directionsDisplay = new google.maps.DirectionsRenderer();
   directionsDisplay.setMap(map);
+  //map.clearOverlays();
   var request = {
     origin: start,
     destination: dest,
     travelMode: "DRIVING",
   };
-  directionsService.route(request, function (result, status) {
-    if (status == "OK") {
-      console.log(result);
-      directionsDisplay.setDirections(result);
-      console.log(result.routes[0].legs[0].distance.text);
-      console.log(result.routes[0].legs[0].duration.text);
-      document.getElementById("distance").innerHTML =
-        "Total Distance: " + result.routes[0].legs[0].distance.text;
-      document.getElementById("duration").innerHTML =
-        "Total Duration: " + result.routes[0].legs[0].duration.text;
+  let directionResult = await getRoute(request);
+  directionsDisplay.setDirections(directionResult);
+  document.getElementById("distance").innerHTML =
+    "Total Distance: " + directionResult.routes[0].legs[0].distance.text;
+  document.getElementById("duration").innerHTML =
+    "Total Duration: " + directionResult.routes[0].legs[0].duration.text;
 
-      //Finding "stops"
-      var steps = result.routes[0].legs[0].steps;
-      var sumDuration = 0;
-      var stops = [];
-      if (steps.length > 1) {
-        for (var i = 0; i < steps.length - 1; i++) {
-          sumDuration += steps[i].duration.value;
-          if (sumDuration + steps[i + 1].duration.value > 7200) {
-            stops.push(steps[i].end_location);
-            sumDuration = 0;
-          } else if (sumDuration >= 5400) {
-            stops.push(steps[i].end_location);
-            sumDuration = 0;
-          }
-        }
+  //Finding "stops"
+  var steps = directionResult.routes[0].legs[0].steps;
+  var sumDuration = 0;
+  var stops = [];
+  if (steps.length > 1) {
+    for (var i = 0; i < steps.length - 1; i++) {
+      sumDuration += steps[i].duration.value;
+      if (sumDuration + steps[i + 1].duration.value > 7200) {
+        stops.push(steps[i].end_location);
+        sumDuration = 0;
+      } else if (sumDuration >= 5400) {
+        stops.push(steps[i].end_location);
+        sumDuration = 0;
       }
-      console.log(stops);
-
-      //Marking "stops"
-      for (var i = 0; i < stops.length; i++) {
-        // new google.maps.Marker({
-        //   map,
-        //   title: "Stop " + i.toString(),
-        //   position: {
-        //     lat: stops[i].lat(),
-        //     lng: stops[i].lng(),
-        //   },
-        // });
-
-        //Getting nearby gas station
-        var stopRequest = {
-          location: new google.maps.LatLng(stops[i].lat(), stops[i].lng()),
-          radius: "16000",
-          type: ["gas_station"],
-          rankby: "distance",
-        };
-        service = new google.maps.places.PlacesService(map);
-        service.nearbySearch(stopRequest, function (results, status) {
-          if (status == google.maps.places.PlacesServiceStatus.OK) {
-            var place = results[0];
-            points.push({
-              lat: place.geometry.location.lat(),
-              lng: place.geometry.location.lng(),
-            });
-            console.log(
-              place.geometry.location.lat(),
-              place.geometry.location.lng()
-            );
-            new google.maps.Marker({
-              map,
-              title: place.name,
-              position: {
-                lat: place.geometry.location.lat(),
-                lng: place.geometry.location.lng(),
-              },
-            });
-          }
-        });
-      }
-
-      console.log(points);
-      //Drawing route
-      points = [
-        { lat: "32.7766642", lng: "-96.79698789999999" },
-        { lat: "29.7604267", lng: "-95.3698028" },
-        { lat: "31.88048749999999", lng: "-96.3472792" },
-        { lat: "30.1420754", lng: "-95.469291" },
-      ];
-      var endPoint = points[1];
-      points.splice(1, 1);
-      points.push(endPoint);
-      console.log(points);
-      var urlParams = "";
-      for (var i = 0; i < points.length; i++) {
-        urlParams += points[i].lat + "," + points[i].lng + "/";
-      }
-      var url = "https://www.google.com/maps/dir/" + urlParams;
-      window.open(url);
     }
-  });
+  }
+
+  for (const stop of stops) {
+    // new google.maps.Marker({
+    //   map,
+    //   title: "Stop " + i.toString(),
+    //   position: {
+    //     lat: stop.lat(),
+    //     lng: stop.lng(),
+    //   },
+    // });
+
+    //Getting nearby gas station
+    var stopRequest = {
+      location: new google.maps.LatLng(stop.lat(), stop.lng()),
+      radius: "16000",
+      type: ["gas_station"],
+      rankby: "distance",
+    };
+    service = new google.maps.places.PlacesService(map);
+    let stopsPoints = await getStops(service, stopRequest);
+    new google.maps.Marker({
+      map,
+      title: stopsPoints.name,
+      position: {
+        lat: stopsPoints.geometry.location.lat(),
+        lng: stopsPoints.geometry.location.lng(),
+      },
+    });
+    points.push({
+      lat: stopsPoints.geometry.location.lat(),
+      lng: stopsPoints.geometry.location.lng(),
+    });
+  }
+
+  let endPoint = await getAddress(dest);
+  points.push({lat: endPoint.geometry.location.lat(), long: endPoint.geometry.location.lng()});
+  
+  var urlParams = "";
+  for (var i = 0; i < points.length; i++) {
+    console.log(points);
+    urlParams += points[i].lat + "," + points[i].lng + "/";
+  }
+  var url = "https://www.google.com/maps/dir/" + urlParams;
+  window.open(url);
 }
 
 function codeAddress() {
